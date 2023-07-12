5be25100b002ba3bcce6c5f50b78b290961ac4e8
NiicoAdasme <nicolas.wladimir@gmail.com>
2023-07-12 00:21:16 -0400

    add scrollbar y toplevel modal

--- tkinter/client/gui_app.py
+++ tkinter/client/gui_app.py
@@ -130,9 +130,9 @@ class Frame(tk.Frame):
         self.button_merge.grid(row=3, column=3, padx=10, pady=10)
 
         # Button export to CSV
-        self.button_export_ih = tk.Button(
-            self, text='Exportar a CSV', command=lambda: self.export_to_csv(tipo='ih', fecha=''))
-        self.button_export_ih.grid(row=3, column=4, padx=10, pady=10)
+        # self.button_export_ih = tk.Button(
+        # self, text='Exportar a CSV', command=lambda: self.export_to_csv(tipo='ih', fecha=''))
+        #self.button_export_ih.grid(row=3, column=4, padx=10, pady=10)
 
     # Functions open file
 
@@ -180,59 +180,60 @@ class Frame(tk.Frame):
             if tipo == 'tmin':
                 path_netcdf = self.entry_tmin.get()
                 msg = 'Por favor, ingrese archivo NetCDF para temperatura minima'
-        
+
             if tipo == 'tmax':
                 path_netcdf = self.entry_tmax.get()
                 msg = 'Por favor, ingrese archivo NetCDF para temperatura maxima'
-            
+
             if tipo == 'pr':
                 path_netcdf = self.entry_pr.get()
                 msg = 'Por favor, ingrese archivo NetCDF para precipitaciones'
 
             if len(path_netcdf) > 0:
                 # Create a modal to show the message
-                self.modal_calendar(tipo= tipo, action= 'plot')
+                self.modal_calendar(tipo=tipo, action='plot')
             else:
-                tk.messagebox.showerror('Ups! Se te ha olvidado el archivo', msg)
+                tk.messagebox.showerror(
+                    'Ups! Se te ha olvidado el archivo', msg)
 
     #  Function export to CSV
-    def export(self, tipo= ['tmin', 'tmax', 'pr', 'ih']):
+    def export(self, tipo=['tmin', 'tmax', 'pr', 'ih']):
         if tipo == 'tmin':
             path_netcdf = self.entry_tmin.get()
             msg = 'Por favor, ingrese archivo NetCDF para temperatura minima'
-        
+
         if tipo == 'tmax':
             path_netcdf = self.entry_tmax.get()
             msg = 'Por favor, ingrese archivo NetCDF para temperatura maxima'
-        
+
         if tipo == 'pr':
             path_netcdf = self.entry_pr.get()
             msg = 'Por favor, ingrese archivo NetCDF para precipitaciones'
-            
+
         if tipo == 'ih':
             ruta_ih = self.entry_ih.get()
             msg = 'Por favor, ingrese archivo NetCDF para el índice hídrico'
 
-            if len(ruta_ih) > 0:            
-                self.modal_calendar(tipo= tipo, action= 'csv')
+            if len(ruta_ih) > 0:
+                self.modal_calendar(tipo=tipo, action='csv')
             else:
-                tk.messagebox.showerror('Ups! Faltan archivos NetCDF', msg) 
-        
+                tk.messagebox.showerror('Ups! Faltan archivos NetCDF', msg)
+
         if len(path_netcdf) > 0:
-            self.modal_calendar(tipo= tipo, action= 'csv')
+            self.modal_calendar(tipo=tipo, action='csv')
         else:
             tk.messagebox.showerror('Ups! Se te ha olvidad el archivo', msg)
 
-
-    def modal_calendar(self, tipo= ['tmin', 'tmax', 'pr', 'ih'], action= ['plot', 'csv']):
+    def modal_calendar(self, tipo=['tmin', 'tmax', 'pr', 'ih'], action=['plot', 'csv']):
         top = tk.Toplevel()
         top.title('Seleccione una fecha')
         top.geometry("780x80")
 
-        lbl = tk.Label(top, text= 'Seleccione una fecha entre 1978-12-15 y 2019-01-01 (Formato: YYYY-MM-DD)')
-        lbl.grid(row= 0, column= 0, padx= 10, pady= 10)
-        cal = DateEntry(top, selectmode= 'day', date_pattern= 'YYYY-mm-dd')
-        cal.grid(row= 0, column= 1, padx= 10, pady= 10)
+        lbl = tk.Label(
+            top, text='Seleccione una fecha entre 1978-12-15 y 2019-01-01 (Formato: YYYY-MM-DD)')
+        lbl.grid(row=0, column=0, padx=10, pady=10)
+        cal = DateEntry(top, selectmode='day', date_pattern='YYYY-mm-dd')
+        cal.grid(row=0, column=1, padx=10, pady=10)
 
         if tipo == 'tmin':
             title = 'Mapa de temperatura minima de '
@@ -241,114 +242,144 @@ class Frame(tk.Frame):
             title = 'Mapa de temperatura maxima de '
 
         if tipo == 'pr':
-            title= 'Mapa de precipitaciones de '
+            title = 'Mapa de precipitaciones de '
 
         if tipo == 'ih':
-            title= 'Mapa de indice de riesgo hidrico de '
+            title = 'Mapa de indice de riesgo hidrico de '
 
         if action == 'plot':
             texto = 'Ver Mapa'
         else:
             texto = 'Exportar a CSV'
 
-        self.button_date = tk.Button(top, text= texto, command= lambda: self.validate_date(fecha= cal.get_date(), 
-                                                                                    titulo= title, tipo= tipo, action= action))
-        self.button_date.grid(row= 0, column= 2, padx= 10, pady= 10)
+        self.button_date = tk.Button(top, text=texto, command=lambda: self.validate_date(fecha=cal.get_date(),
+                                                                                         titulo=title, tipo=tipo, action=action))
+        self.button_date.grid(row=0, column=2, padx=10, pady=10)
 
         top.mainloop()
-    
+
     # validate date in a valid range
-    def validate_date(self, fecha, titulo, tipo = ['tmin', 'tmax', 'pr', 'ih'], action= ['plot', 'csv']):
+    def validate_date(self, fecha, titulo, tipo=['tmin', 'tmax', 'pr', 'ih'], action=['plot', 'csv']):
         start_date = datetime.date(1978, 12, 15)
         ending_date = datetime.date(2019, 10, 30)
         if fecha < start_date or fecha > ending_date:
-            return tk.messagebox.showerror('Ups! Fecha fuera de rango', 
-                                            'Por favor, ingrese una fecha entre 1978-12-15 y 2019-10-30')
+            return tk.messagebox.showerror('Ups! Fecha fuera de rango',
+                                           'Por favor, ingrese una fecha entre 1978-12-15 y 2019-10-30')
         else:
             if action == 'plot':
-                self.modal_map(titulo= titulo, fecha= fecha, tipo= tipo)
+                self.modal_map(titulo=titulo, fecha=fecha, tipo=tipo)
             else:
-                self.export_to_csv(fecha= fecha, tipo= tipo)
+                self.export_to_csv(fecha=fecha, tipo=tipo)
 
     # show image
-    def modal_map(self, titulo, fecha, tipo= ['tmin', 'tmax', 'pr', 'ih']):
+    def modal_map(self, titulo, fecha, tipo=['tmin', 'tmax', 'pr', 'ih']):
         if tipo == 'tmin':
-            path_netcdf= self.entry_tmin.get()
-        
+            path_netcdf = self.entry_tmin.get()
+
         if tipo == 'tmax':
-            path_netcdf= self.entry_tmax.get()
+            path_netcdf = self.entry_tmax.get()
 
         if tipo == 'pr':
-            path_netcdf= self.entry_pr.get()
+            path_netcdf = self.entry_pr.get()
 
         if tipo == 'ih':
             ruta_ih = self.entry_ih.get()
             # plot map ih
 
-        res = plot(ruta= path_netcdf, fecha= fecha, tipo= tipo)
+        res = plot(ruta=path_netcdf, fecha=fecha, tipo=tipo)
+
         image = ImageTk.PhotoImage(Image.open(res))
         top = tk.Toplevel()
         top.title(titulo + str(fecha))
-        top.geometry("800x850")
-        lbl = tk.Label(top, image= image)
-        lbl.grid(row= 0, column= 0)
-        tk.messagebox.showinfo('Imagen Guardada', 'La imagen se guardó correctamente en '+ str(res))
+        # top.geometry("800x850")
+        top.attributes('-fullscreen', False)
+        scrollbar = tk.Scrollbar(top)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # lbl = tk.Label(top, image= image)
+        # lbl.grid(row= 0, column= 0)
+        # lbl.pack()
+
+        canvas = tk.Canvas(
+            top,
+            width=1000,
+            height=800
+        )
+
+        canvas.pack()
+        canvas.create_image(
+            10,
+            10,
+            anchor=tk.NW,
+            image=image
+        )
+        scrollbar.config(command=canvas.yview)
+        
+        tk.messagebox.showinfo(
+            'Imagen Guardada', 'La imagen se guardó correctamente en ' + str(res))
         top.mainloop()
 
     # geenerate gif
-    def generate_gif(self, tipo= ['tmin', 'tmax', 'pr']):
+    def generate_gif(self, tipo=['tmin', 'tmax', 'pr']):
         if tipo == 'tmin':
-            path_netcdf= self.entry_tmin.get()
-        
+            path_netcdf = self.entry_tmin.get()
+
         if tipo == 'tmax':
-            path_netcdf= self.entry_tmax.get()
+            path_netcdf = self.entry_tmax.get()
 
         if tipo == 'pr':
-            path_netcdf= self.entry_pr.get()
-        
-        res = gen_timelapse(ruta= path_netcdf, tipo= tipo)
-        
-        print('respuesta del timelapse: '+ res)
+            path_netcdf = self.entry_pr.get()
+
+        res = gen_timelapse(ruta=path_netcdf, tipo=tipo)
+
+        print('respuesta del timelapse: ' + res)
 
         if len(res) > 0:
-            tk.messagebox.showinfo('GIF Guardado', 'El GIF se guardó correctamente en '+ res)
+            tk.messagebox.showinfo(
+                'GIF Guardado', 'El GIF se guardó correctamente en ' + res)
         else:
-            tk.messagebox.showerror('Ups! Error al generar GIF', 'Lo sentimos. Hubo un error al generar el GIF')
+            tk.messagebox.showerror(
+                'Ups! Error al generar GIF', 'Lo sentimos. Hubo un error al generar el GIF')
 
     # export to csv
-    def export_to_csv(self, fecha, tipo= ['tmin', 'tmax', 'pr', 'ih']):
+    def export_to_csv(self, fecha, tipo=['tmin', 'tmax', 'pr', 'ih']):
         if tipo == 'tmin':
-            path_netcdf= self.entry_tmin.get()
-        
+            path_netcdf = self.entry_tmin.get()
+
         if tipo == 'tmax':
-            path_netcdf= self.entry_tmax.get()
+            path_netcdf = self.entry_tmax.get()
 
         if tipo == 'pr':
-            path_netcdf= self.entry_pr.get()
+            path_netcdf = self.entry_pr.get()
 
         if tipo == 'ih':
             ruta_ih = self.entry_ih.get()
             # ! EXPORTAR INDICE HIDRCO A CSV
-            res = ih_export_csv(ruta= ruta_ih)
+            res = ih_export_csv(ruta=ruta_ih)
             # ! IMPORTANTE
         else:
             res = export_csv(path_netcdf, fecha, tipo)
 
         if res == None:
-            tk.messagebox.showinfo('Exportado a CSV', 'Archivo CSV guardado correctamente en la carpera CSV')
+            tk.messagebox.showinfo(
+                'Exportado a CSV', 'Archivo CSV guardado correctamente en la carpera CSV')
         else:
-            tk.messagebox.showerror('Ups! Error al exportar', 'Lo sentimos. No se pudo exportar a CSV')
+            tk.messagebox.showerror(
+                'Ups! Error al exportar', 'Lo sentimos. No se pudo exportar a CSV')
 
-    def merge_netcdf(self, tmin , tmax, pr):
-        res, output_path = combinar_netcdf(netcdf_tmin= tmin, netcdf_tmax= tmax, netcdf_pr= pr)
+    def merge_netcdf(self, tmin, tmax, pr):
+        res, output_path = combinar_netcdf(
+            netcdf_tmin=tmin, netcdf_tmax=tmax, netcdf_pr=pr)
 
         print(res, output_path)
 
         if res == None:
-            tk.messagebox.showinfo('Merge exitoso', 'Se combinaron los tres archivos NetCDF de forma exitosa en la carpeta NetCDF')
-            return self.modal_ih(ruta_netcdf_merged= output_path)
+            tk.messagebox.showinfo(
+                'Merge exitoso', 'Se combinaron los tres archivos NetCDF de forma exitosa en la carpeta NetCDF')
+            return self.modal_ih(ruta_netcdf_merged=output_path)
         else:
-            tk.messagebox.showerror('Ups! Error al combinar', 'Lo sentimos. No se pudo combinar los archivos NetCDF')
+            tk.messagebox.showerror(
+                'Ups! Error al combinar', 'Lo sentimos. No se pudo combinar los archivos NetCDF')
             return False
 
     def validarCampos(self):
@@ -357,27 +388,31 @@ class Frame(tk.Frame):
         ruta_pr = self.entry_pr.get()
 
         ruta_actual = os.getcwd()
-        ruta_completa = os.path.join(ruta_actual, 'netcdf', 'archivo_combinado.nc')
+        ruta_completa = os.path.join(
+            ruta_actual, 'netcdf', 'archivo_combinado.nc')
 
         if len(ruta_tmin) > 0 and len(ruta_tmax) > 0 and len(ruta_pr) > 0:
             # validar que exista el archivo combinado. caso contrario, crearlo
             if os.path.isfile(ruta_completa):
                 # modal ih
-                return self.modal_ih(ruta_netcdf_merged= ruta_completa)
+                return self.modal_ih(ruta_netcdf_merged=ruta_completa)
             else:
-                self.merge_netcdf(tmin= ruta_tmin, tmax= ruta_tmax, pr= ruta_pr)
-        else: 
+                self.merge_netcdf(tmin=ruta_tmin, tmax=ruta_tmax, pr=ruta_pr)
+        else:
             return tk.messagebox.showerror('Ups! Faltan archivos NetCDF', 'Se deben ingresar los archivos tmin, tmax y pr NetCDF para combinarlos ⚠')
 
     # ? DEFINIR FUNCION PARA CREAR NETCDF POR ZONA Y FECHA (MES) SELECCIONADA
     def ih_to_netcdf(self, ruta, fecha, zona):
 
-        res = export_ih_to_netcdf(ruta_nc_combinado= ruta, fecha= fecha, zona= zona)
+        res = export_ih_to_netcdf(
+            ruta_nc_combinado=ruta, fecha=fecha, zona=zona)
 
         if len(res) > 0:
-            tk.messagebox.showinfo('NetCDF Creado', 'Se creó el archivo NetCDF de forma exitosa en la carpeta NetCDF')
+            tk.messagebox.showinfo(
+                'NetCDF Creado', 'Se creó el archivo NetCDF de forma exitosa en la carpeta NetCDF')
         else:
-            tk.messagebox.showerror('Ups! Error al crear NetCDF', 'Lo sentimos. No se pudo crear el archivo NetCDF')
+            tk.messagebox.showerror(
+                'Ups! Error al crear NetCDF', 'Lo sentimos. No se pudo crear el archivo NetCDF')
 
     def validar_fecha(self, fecha, zona, ruta):
         start_date = datetime.date(1978, 12, 15)
@@ -392,16 +427,18 @@ class Frame(tk.Frame):
         top = tk.Toplevel()
         top.title('Seleccione una fecha y zona')
         top.geometry("500x180")
+        top.attributes('-fullscreen', False)
 
         # Fecha
-        lbl = tk.Label(top, text= 'Seleccione una fecha (1978-12-15 y 2019-01-01)')
-        lbl.grid(row= 0, column= 0, padx= 10, pady= 10)
-        cal = DateEntry(top, selectmode= 'day', date_pattern= 'YYYY-mm-dd')
-        cal.grid(row= 0, column= 1, padx= 10, pady= 10)
-        
+        lbl = tk.Label(
+            top, text='Seleccione una fecha (1978-12-15 y 2019-01-01)')
+        lbl.grid(row=0, column=0, padx=10, pady=10)
+        cal = DateEntry(top, selectmode='day', date_pattern='YYYY-mm-dd')
+        cal.grid(row=0, column=1, padx=10, pady=10)
+
         # Zona
-        lbl = tk.Label(top, text= 'Seleccione una zona')
-        lbl.grid(row= 1, column= 0, padx= 10, pady= 10)
+        lbl = tk.Label(top, text='Seleccione una zona')
+        lbl.grid(row=1, column=0, padx=10, pady=10)
 
         OPTIONS = [
             "12",
@@ -413,29 +450,80 @@ class Frame(tk.Frame):
         ]
 
         variable = tk.StringVar(top)
-        variable.set(OPTIONS[0]) # default value
+        variable.set(OPTIONS[0])  # default value
 
         dropdown = tk.OptionMenu(top, variable, *OPTIONS)
-        dropdown.grid(row= 1, column= 1, padx= 10, pady= 10)
+        dropdown.grid(row=1, column=1, padx=10, pady=10)
 
-        self.button_date = tk.Button(top, text= 'Exportar a NetCDF', command= lambda: self.validar_fecha(fecha= cal.get_date(), zona= variable.get(),ruta= ruta_netcdf_merged))
-        self.button_date.grid(row= 2, column= 1, padx= 10, pady= 10)
+        self.button_date = tk.Button(top, text='Exportar a NetCDF', command=lambda: self.validar_fecha(
+            fecha=cal.get_date(), zona=variable.get(), ruta=ruta_netcdf_merged))
+        self.button_date.grid(row=2, column=1, padx=10, pady=10)
 
         top.mainloop()
 
     # ? DEFINIR FUNCION PARA MOSTRAR EL MAPA DE LA ZONA Y FECHA (MES) SELECCIONADA
     def show_ih(self, ruta_netcdf_fecha_zona):
-        res = mapa_ih(ruta_netcdf= ruta_netcdf_fecha_zona)
+        res = mapa_ih(ruta_netcdf=ruta_netcdf_fecha_zona)
 
         if res != False:
             # exitoso. ruta de la imagen
-            tk.messagebox.showinfo('Mapa IH', 'Se creó el mapa IH de forma exitosa en la carpeta img')
+            # Redirigir a un modal con la img
+            self.modal_ih_map(res)
+            tk.messagebox.showinfo(
+                'Mapa IH', 'Se creó el mapa IH de forma exitosa en la carpeta img')
         else:
             # error
-            tk.messagebox.showerror('Ups! Error al crear mapa IH', 'Lo sentimos. No se pudo crear el mapa IH')
-             
+            tk.messagebox.showerror(
+                'Ups! Error al crear mapa IH', 'Lo sentimos. No se pudo crear el mapa IH')
+
+    def modal_ih_map(self, imagePath):
+        # Load the image
+        # image= Image.open(imagePath)
+
+        # Resize the image in the given (width, height)
+        # imag=image.resize((1000, 800))
+
+        # Conver the image in TkImage
+        # img=ImageTk.PhotoImage(imag)
+
+        img = ImageTk.PhotoImage(Image.open(imagePath).resize((1920, 960)))
+        # img = tk.PhotoImage(file=imagePath) 
+
+        top = tk.Toplevel()
+        top.title('Mapa Índice de Riesgo Hídrico por Zona')
+        top.attributes('-fullscreen', False)
+
+        scrollbar = tk.Scrollbar(top)
+        scrollbar2 = tk.Scrollbar(top, orient='horizontal')
+        
+        scrollbar.pack( side = tk.RIGHT, fill = tk.Y )
+        scrollbar2.pack( side = tk.BOTTOM, fill = tk.X )
+
+        # lbl = tk.Label(
+        #         top,
+        #         image=img
+        #     )
+
+        # lbl.pack( side = tk.LEFT, fill = tk.Y )
+
+        canvas = tk.Canvas(
+                top,
+                width = 1500,
+                height = 800
+            )
+        canvas.pack()
+        canvas.create_image(
+            10,
+            10,
+            anchor=tk.NW,
+            image=img
+        )
+
+        scrollbar.config( command = canvas.yview )
+        scrollbar2.config( command= canvas.xview )
+
+        top.mainloop()
+
     # ? DEFINIR FUNCION PARA EXPORTAR A CSV LA ZONA Y FECHA (MES) SELECCIONADA
 
     # ? DEFINIR FUNCION PARA GENERAR GIF DE LA ZONA Y FECHA (MES) SELECCIONADA
-
-    
